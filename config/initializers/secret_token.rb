6e4e35f6fcbb27a7a036d0db5bfb66f5877673a6
amrit <amrit@lenovo.(none)>
2013-07-05 10:52:27 +0545

    Initial project and gem required for tdd

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuardSpork::Application.config.secret_token = 'a07391929f0fe92dcca8116f6136070d8252d752053da47c379dc82c11c17c6817603ae9039b2bed2b2b2a370e3e77e3cb4138d615fba48392a0f5c3542b6ee3'
